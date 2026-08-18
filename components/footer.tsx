b93c54c0b1e978e179c65bc54c813babc40c1541
mshrynzw <iq87io25@gmail.com>
2026-08-18 17:07:14 +0900

    fix

--- components/footer.tsx
+++ components/footer.tsx
@@ -11,7 +11,7 @@ function TiktokIcon(props: React.SVGProps<SVGSVGElement>) {
 }
 
 const socials = [
-  { label: "YouTube", href: "https://www.youtube.com/playlist?list=PLNODum9w1rr8", Icon: Youtube },
+  { label: "YouTube", href: "https://www.youtube.com/watch?v=XDmU2NEbnZk&list=PLGTHA_agRTUU", Icon: Youtube },
   { label: "TikTok", href: "https://www.tiktok.com/@kaoru_yuki_sdw", Icon: TiktokIcon },
 ];
 
